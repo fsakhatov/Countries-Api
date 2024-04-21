8aa026ab839968adc21a4c8a4323c635d9f36c96
farrukhbek26 <FSAKHATOV@GMAIL.COM>
2024-04-21 14:08:20 +0500

    Filter by region

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const countryElem = document.querySelector('.countries');
 const dropDown = document.querySelector('.dropDown');
 const dropElem = document.querySelector('.drop')
+const region = document.querySelectorAll('.region');
+const search = document.querySelector('.searchInput');
 
 
 async function getCountry(){
@@ -21,13 +23,29 @@ function showCountry(data){
 </div>
 <div class="country-info">
    <h5>${data.name.common}</h5>
-   <p><strong>Population: </strong>${data.population}</p>
-   <p><strong>Region: </strong>${data.region}</p>
+   <p><strong>Population: </strong>${data.population.toLocaleString()}</p>
+   <p class="regionName"><strong>Region: </strong>${data.region}</p>
    <p><strong>Capital: </strong>${data.capital}</p>
 </div>`
 countryElem.appendChild(country)
 }
 dropDown.addEventListener('click', () => {
    dropElem.classList.toggle('showDropDown')
-   console.log();
 })
+
+const regionName = document.getElementsByClassName('regionName');
+region.forEach(element => {
+    element.addEventListener('click', () => {
+            Array.from(regionName).forEach(elem => {
+            if( elem.innerText.includes(element.innerText) || element.innerText == 'All' ){
+                elem.parentElement.parentElement.style.display = 'grid'
+            }
+            else{
+                elem.parentElement.parentElement.style.display = 'none' 
+            }
+            })
+        })
+    })
+    search.addEventListener('input', () => {
+   console.log(search.value);
+    })
